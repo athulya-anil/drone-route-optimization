db3346895656e059715f4e0bf2f6f2ef05a4b4dc
Varsha R <varsharavichandran6@gmail.com>
2024-12-04 21:43:19 -0500

    weather and traffic APIs

--- try-1/data_ingestion.py
+++ try-1/data_ingestion.py
@@ -2,20 +2,41 @@ from kafka import KafkaProducer
 import json
 import time
 import requests
+import random
 
 # Kafka Producer configuration
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 def fetch_traffic_data():
     # Replace with actual traffic API
-    response = requests.get('https://api.trafficdata.com')
-    return response.json()
+    # response = requests.get('https://api.trafficdata.com')
+
+    # return response.json()
+    
+    headers = {
+    "Content-Type": "application/json"
+    }
+    username = "userxyz123"
+    password = "091mYxuQCx"
+
+    response = requests.post("https://run.mocky.io/v3/bddd16d8-7c5f-466e-9e11-75463c263bdb",auth=(username, password), headers=headers)
+    selection = random.randint(1,3)
+    traffic_response = response.json()['data'][str(selection)]
+
+    return traffic_response
 
 def fetch_weather_data():
-    # Replace with actual weather API
-    # response = requests.get('https://api.weatherdata.com')
-    response = requests.get('api.meteomatics.com/2024-12-04T00:00:00Z--2024-12-07T00:00:00Z:PT1H/t_2m:C/52.520551,13.461804/json')
-    return response.json()
+    headers = {
+    "Content-Type": "application/json"
+    }
+    username = "userxyz123"
+    password = "091mYxuQCx"
+
+    response = requests.post("https://run.mocky.io/v3/db8f7b49-3171-46dc-b06d-2ef82122a667",auth=(username, password), headers=headers)
+    selection = random.randint(1,7)
+    weather_response = response.json()['data'][str(selection)]
+
+    return weather_data
 
 while True:
     traffic_data = fetch_traffic_data()
